8e15bc3b07b62e87d5302d93c188441a6b647beb
Alfitopasaribu <alfitopasaribu@gmail.com>
2026-06-07 19:17:43 +0800

    update 11

--- frontend/lib/core/constants/app_constants.dart
+++ frontend/lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'https://DOMAIN-BACKEND-KAMU.vercel.app/api';
+  static const String baseUrl = 'https://komplek-guard-ai.vercel.app/api';
 
   static const double defaultLat = -8.650000;
   static const double defaultLng = 115.216667;
@@ -8,4 +8,3 @@ class AppConstants {
   static const String tokenKey = 'auth_token';
   static const String userKey = 'user_data';
 }
-
